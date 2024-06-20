49974e2d02352c762df41eb6de2160167abb8572
nilesh2302 <nileshchaudhari2302@gmail.com>
2024-06-20 12:42:38 +0530

    Updates

---
+++ precache-manifest.b4556e8bd6acedb88317d589870e1bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f097c72e96a0165109e0f335be62195",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "fce068173afca6e13ca0",
+    "url": "/WeatherApp/static/css/main.4b2f6253.chunk.css"
+  },
+  {
+    "revision": "b3c40c76f4e64c2c7d57",
+    "url": "/WeatherApp/static/js/2.732d2c9b.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/WeatherApp/static/js/2.732d2c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce068173afca6e13ca0",
+    "url": "/WeatherApp/static/js/main.34737389.chunk.js"
+  },
+  {
+    "revision": "1292c128c0053fc8bdeb",
+    "url": "/WeatherApp/static/js/runtime-main.8429af3f.js"
+  }
+]);
